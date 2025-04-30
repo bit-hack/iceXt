ee91e48e7183a29699821a9443e581e64e91896d
Aidan <aidan.dodds@googlemail.com>
2025-05-01 00:13:29 +0100

    Remove old boot

--- misc/boot/build.bat
+++
@@ -1,4 +0,0 @@
-nasm\nasm.exe boot.asm
-python tohex.py > program.hex
-REM del boot
-set /p DUMMY=Hit ENTER to continue...
--- misc/boot/tohex.py
+++
@@ -1,3 +0,0 @@
-with open('boot', 'rb') as fd:
-    for x in fd.read():
-        print('{:02x}'.format(x))
