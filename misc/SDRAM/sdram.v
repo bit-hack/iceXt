2209f44af1da89928c64990c1ed671d4773b7747
Aidan <aidan.dodds@googlemail.com>
2025-06-06 21:44:12 +0100

    sdram updates

--- misc/SDRAM/sdram.v
+++ misc/SDRAM/sdram.v
@@ -1,11 +1,7 @@
 `default_nettype none
 
 
-//
-
-
 module sdram(
-
   input         iClkCpu,
   input         iClkSdram,
   input         iRst,
@@ -14,7 +10,7 @@ module sdram(
   // cpu interface
   //
 
-  input  []
+  input  [23:0] iAddr,
   input  [15:0] iWrData,
   output [15:0] oRdData,
   input  [ 1:0] iWr,
@@ -39,6 +35,33 @@ module sdram(
   output reg        oSdramDqEn  = 0,    // data out enable
   output reg [ 1:0] oSdramDqm   = 0     // byte select
 );
+
+  // banks| 4         bits| 2
+  // row  | 8192      bits| 13
+  // col  | 512       bits| 9
+  wire bank   = iAddr[23:22];
+  wire row    = iAddr[21: 9];
+  wire column = iAddr[ 8: 0];
+
+  //                         /RAS /CAS /WE
+  localparam CMD_NOP       = 3'b1_1_1;
+  localparam CMD_READ      = 3'b1_0_1;
+  localparam CMD_WRITE     = 3'b1_0_0;
+  localparam CMD_PRECHARGE = 3'b0_1_0;
+  localparam CMD_ACTIVATE  = 3'b0_1_1;
+  localparam CMD_REFRESH   = 3'b0_0_1;
+  localparam CMD_MODE_SEL  = 3'b0_0_0;
+
+
+  // at 1mhz, the period is exactly 1us
+
+  localparam wait_200us   = ;
+  localparam wait_CAS     = 2;
+  localparam wait_refresh = ;
+  localparam wait_tMRD    = ;
+  localparam wait_tRC     = ;
+  localparam wait_tRCD    = ;
+  localparam wait_tRP     = ;
 
   reg  [ 3:0] state;
   reg  [15:0] delay;
@@ -65,7 +88,7 @@ module sdram(
         // DQ input by default
         oSdramDqEn <= 0;
 
-        case (state) begin
+        case (state)
 
         //
         // reset
@@ -78,18 +101,19 @@ module sdram(
             sdramSync <= 0;
         end
         4'd1: begin
-            // issue precharge (all banks)
-            cmd   <= CMD_PRECHARGE;
-            delay <= wait_tRP;
+            // issue pre-charge
+            cmd         <= CMD_PRECHARGE;
+            oSdramA[10] <= 1'b1;  // all banks
+            delay       <= wait_tRP;
         end
         4'd2, 4'd3, 4'd4, 4'd5, 4'd6, 4'd7, 4'd8, 4'd9: begin
             // eight auto refresh cycles
-            cmd   <= CMD_AUTO_REFRESH;
+            cmd   <= CMD_REFRESH;
             delay <= wait_tRC;
         end
         4'd3: begin
             // load mode register
-            cmd   <= CMD_MODE_SEL;
+            cmd <= CMD_MODE_SEL;
             { oSdramBa, oSdramA } <= {
                 5'd0,       // reserved
                 1'b1,       // single location access
@@ -110,15 +134,15 @@ module sdram(
             state <= 4'd4;
             // issue auto refresh if needed
             if (refresh == 0) begin
-              cmd     <= CMD_AUTO_REFRESH;
+              cmd     <= CMD_REFRESH;
               delay   <= wait_tRC;
               refresh <= wait_refresh;
             end else begin
               if (cpuSync != sdramSync) begin
                   if ((iWr != 0) | (iRd != 0)) begin
                     cmd      <= CMD_ACTIVATE;
-                    oSdramA  <= // ROW
-                    oSdramBa <= // BANK
+                    oSdramA  <= row;  // 13 bits
+                    oSdramBa <= bank; // 2 bits
                     delay    <= wait_tRCD;
                   end
               end
@@ -131,13 +155,15 @@ module sdram(
 
         4'd5: begin
 
-            { oSdramA[12:0] <= { 4'b1111, /*COLUMN*/ };
-            // set byte enables
-            oSdramDqm <= 2'b00;  // active low?
+            oSdramA[12:0] <= {
+              4'b1111,
+              column    // 9 bits
+            };
 
             if (iRd) begin
                 cmd        <= CMD_READ;
                 delay      <= wait_CAS;
+                oSdramDqm  <= 2'b00;
                 // goto read handler
                 state      <= 4'd6;
             end
@@ -146,6 +172,8 @@ module sdram(
                 delay      <= tDPL + tRP;
                 // goto to idle state
                 state      <= 4'd4;
+                // byte select (active low)
+                oSdramDqm  <= ~iWr;
                 // present data
                 oSdramDqEn <= 1'b1;
                 oSdramDq   <= /*data to write*/
@@ -161,6 +189,7 @@ module sdram(
         4'd6: begin
             delay <= 1;  // (wait_tRP + 1) - wait_CAS
             // read in data from SDRAM
+            oRdData <= iSdramDq;
         end
         4'd7: begin
             state <= 4'd4;  // IDLE state
